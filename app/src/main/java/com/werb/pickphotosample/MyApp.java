6b9310af59550538980b2f528085aa6a1725324d
wanbo <werbhelius@gmail.com>
2019-01-02 16:38:28 +0800

    remove leakcanary

--- app/src/main/java/com/werb/pickphotosample/MyApp.java
+++ app/src/main/java/com/werb/pickphotosample/MyApp.java
@@ -12,13 +12,6 @@ public class MyApp extends Application {
 
     @Override public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
-        // Normal app init code...
     }
 
 }
